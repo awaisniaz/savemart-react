887036e4630e4d760a0975238a5a3df2626edccd
Awais Niaz <awais.niaz@mwanmobile.org>
2022-10-27 16:32:25 +0500

    Remove error

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Header from './components/header/header';
 import './App.css';
 
-import Routing from './components/router.js'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
-      <Routing />
     </div>
   );
 }
