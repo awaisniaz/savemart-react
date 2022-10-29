57130cfaeae4cbb7085a2a0dc66221ae4587e343
Awais Niaz <awais.niaz@mwanmobile.org>
2022-10-29 12:06:50 +0500

    Add a login and Signup button

--- src/components/productcard/index.tsx
+++ src/components/productcard/index.tsx
@@ -1,20 +1,23 @@
-import { Card, } from 'antd';
+
 import './styles.css'
 import { products } from '../../configurations/product';
 
-
-
-const { Meta } = Card;
 const Productcard: React.FC = () => (
     <div className='container'>
         {products?.map((item: any, index: any) => (
-            <Card
-                hoverable
-                style={{ width: 300 }}
-                cover={<img alt="example" src={item?.image} />}
-            >
-                <Meta title={item?.Name} description={item?.Name} />
-            </Card>
+            <div className="card">
+
+                <div className="imgBox">
+                    <img src="https://www.corsair.com/corsairmedia/sys_master/productcontent/CH-9300011-NA-M65_PRO_RGB_BLK_04.png" alt="mouse corsair" className="mouse" />
+                </div>
+
+                <div className="contentBox">
+                    <h3>Mouse Corsair M65</h3>
+                    <h2 className="price">61.<small>98</small> €</h2>
+                    <a href="/" className="buy">Buy Now</a>
+                </div>
+
+            </div>
         ))}
     </div>
 
